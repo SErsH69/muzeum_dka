af3ecdb1551179fdff67b97f78da75f87aa6d1ac
Goncharov Sergey <doimpos@gmail.com>
2025-11-10 12:14:59 +0500

    fix blocks

---
+++ src/blocks/modules/fslider_block/fslider_block.js
@@ -0,0 +1,39 @@
+import $ from 'jquery';
+import 'slick-carousel';
+
+const FsliderBlock = class FsliderBlock {
+    constructor(){}
+    initSlider() {
+        document.addEventListener('DOMContentLoaded', () => {
+            $('.js_fslider').slick({
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                arrows: false,
+                infinite: false,
+                dots: false,
+                responsive: [
+                    {
+                        breakpoint: 1023,
+                        settings: {
+                            slidesToShow: 2,
+                            infinite: true
+                        }
+                    },
+                    {
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                            infinite: true
+                        }
+                    }
+                ]
+            });
+        });
+    }
+    init() {
+        this.initSlider();
+    }
+}
+
+export default FsliderBlock;
--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,7 @@ import HistoryMain from '../blocks/modules/history_main/history_main.js';
 import MainVideo from '../blocks/modules/main_video/main_video.js';
 import MainMedal from '../blocks/modules/main_medal/main_medal.js';
 import MainSingle from '../blocks/modules/main_single/main_single.js';
+import FsliderBlock from '../blocks/modules/fslider_block/fslider_block.js';
 import Modals from '../blocks/modules/modals/modals.js';
 
 window.app = new Vue({
@@ -25,6 +26,7 @@ window.app = new Vue({
         historyMain: new HistoryMain(),
         mainVideo: new MainVideo(),
         mainMedal: new MainMedal(),
+        fsliderBlock: new FsliderBlock(),
         mainSingle: new MainSingle(),
         modals: new Modals({
             modalsSelector: "data-modal",
@@ -43,6 +45,7 @@ window.app = new Vue({
         this.historyMain.init();
         this.mainVideo.init();
         this.mainMedal.init();
+        this.fsliderBlock.init();
         this.mainSingle.init();
         this.modals.init();
     },
