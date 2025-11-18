9a637b031d7732e29630c7173354f80b4b1c9d43
Goncharov Sergey <doimpos@gmail.com>
2025-11-18 21:37:42 +0500

    add links

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,19 +1,31 @@
 const Header = class Header {
-    constructor({isMobileMenuOpened}){
+    constructor({ isMobileMenuOpened }) {
         this.isMobileMenuOpened = false;
+        this.html = document.documentElement; // <html>
     }
+
     toogleMobileMenu() {
         this.isMobileMenuOpened = !this.isMobileMenuOpened;
+
+        if (this.isMobileMenuOpened) {
+            this.html.classList.add('hidden');
+        } else {
+            this.html.classList.remove('hidden');
+        }
     }
+
     closeMobileMenu() {
         this.isMobileMenuOpened = false;
+        this.html.classList.remove('hidden');
     }
+
     setEventListener() {
         document.addEventListener('click', (event) => {
             if (event.target.closest('.header__burger--body') || event.target.closest('.header__burger')) return;
             this.closeMobileMenu();
-        }) 
+        });
     }
+
     init() {
         this.setEventListener();
     }
